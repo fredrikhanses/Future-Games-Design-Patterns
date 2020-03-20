641f08f6cee410c68093cfe19c8a2444bf9ff061
Ederic Polo <ederic.polo@maginteractive.se>
2020-03-20 14:38:01 +0100

    First Commit

---
+++ Assets/Scripts/Tests/TowerDefense/TowerDefense_Unit_Tests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+namespace Tests
+{
+    public class TowerDefense_Unit_Tests
+    {
+        [Test]
+        [TestCase("map_1",  0, 2, 2, 0, 18)]
+        [TestCase("map_2",  24, 0, 9, 9, 118)]
+        public void Djistra_Solves_Path(string map, int x0, int y0, int x1, int y1, int result)
+        {
+            Assert.AreEqual(true, true);            
+        }
+    }
+}
